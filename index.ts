81d2760765c51e0d40fc5613904fbb3e367fecf7
Victor <v1sscardoso@gmail.com>
2022-12-02 04:33:07 -0300

    add filters

--- index.ts
+++ index.ts
@@ -2,20 +2,11 @@
 import dotenv from 'dotenv';
 import needle from 'needle';
 
-
 (async ()=>{
   dotenv.config()
 
   const token = process.env.TT_BEARER || ''
-  const appKey = process.env.TT_API_KEY || ''
-  const appSecret = process.env.TT_SECRET || ''
-  const streamURL = 'https://api.twitter.com/2/tweets/search/stream';
-
-  // console.log({bearer});
-  
-  // const twiterClient = new TwitterApi(bearer)
-  
-  // await twiterClient.v2.tweet('test')
+  const streamURL = 'https://api.twitter.com/2/tweets/search/stream?expansions=referenced_tweets.id&tweet.fields=attachments';
 
   const stream = needle.get(streamURL, {
     headers: {
@@ -26,11 +17,11 @@ import needle from 'needle';
 });
 
 stream.on('data', data => {
-  const parsed = data.toString('utf-8');
-  console.log(parsed);
+  if(data.length != 2) {
+    const parsed = JSON.parse(data.toString('utf-8'));
+    console.log(parsed);
     
+  }
 }).on('err', error => {
-    console.log(error);
     })
-
 })()
