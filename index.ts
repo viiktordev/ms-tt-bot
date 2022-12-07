27f435e4e0f179da79f0eb36151865170b85f888
Victor <v1sscardoso@gmail.com>
2022-12-07 04:02:30 -0300

    sending tt events to sqs

--- index.ts
+++ index.ts
@@ -2,32 +2,48 @@
 import dotenv from 'dotenv';
 import needle from 'needle';
 import { pipeline } from 'node:stream/promises';
-import { Transform } from 'node:stream';
+import { Transform, Writable } from 'node:stream';
+import { SQS } from 'aws-sdk';
+
+dotenv.config();
+
+import { stickerQueueUrl, token, streamURL } from './config';
 
 (async ()=>{
-  dotenv.config()
-
-  const token = process.env.TT_BEARER || ''
-  const streamURL = 'https://api.twitter.com/2/tweets/search/stream?expansions=referenced_tweets.id&tweet.fields=attachments';
+  const sqs = new SQS({ apiVersion: '2012-11-05', region: 'us-east-1' });
 
   const stream = needle.get(streamURL, {
     headers: {
-        "User-Agent": "v2FilterStreamJS",
-        "Authorization": `Bearer ${token}`
+        'User-Agent': 'v2FilterStreamJS',
+        'Authorization': `Bearer ${token}`
     },
     timeout: 2000
-});
+  });
 
-const transform = new Transform({
-  transform(chunk, enc, callback) {
-    const jsonData = chunk.toString();
-    console.log(jsonData);
-    return callback(null, jsonData)
-  }
-})
+  const transform = new Transform({
+    transform(chunk, enc, callback) {
+      const jsonData = chunk.toString();
+      console.log(jsonData);
+      return callback(null, jsonData)
+    }
+  });
 
-await pipeline(
-  stream,
-  transform
-)
+  const sendToQueue = new Writable({
+    async write(chunk, enc, callback) {
+      if(chunk.length > 2) {
+        await sqs.sendMessage({
+          QueueUrl: stickerQueueUrl,
+          MessageBody: chunk.toString('utf-8')
+        }).promise();
+      }
+
+      callback()
+    }
+  });
+
+  await pipeline(
+    stream,
+    transform,
+    sendToQueue
+  );
 })()
